5644e3759e014b76cf63f2a5e9e126533dfdee40
DrgX89 <157446206+DrgX89@users.noreply.github.com>
2024-11-01 12:54:31 +0000

    Cambios

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const socket = io();
 const chatForm = document.getElementById('chat-form');
 const chatInput = document.getElementById('chat-input');
 const chatMessages = document.getElementById('chat-messages');
@@ -5,8 +6,13 @@ const chatMessages = document.getElementById('chat-messages');
 chatForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const message = chatInput.value;
-  const newMessage = document.createElement('p');
-  newMessage.textContent = message;
-  chatMessages.appendChild(newMessage);
+  socket.emit('chat message', message);   
+ // Enviar mensaje al servidor
   chatInput.value = '';
+});
+
+socket.on('chat message', (msg) => {
+  const newMessage = document.createElement('p');
+  newMessage.textContent = msg;
+  chatMessages.appendChild(newMessage);
 });
